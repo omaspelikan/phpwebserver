2fcaf077c23af5ecc28f58c921741c61fc47bb57
陳昶文 <mason@Mason-MBP.local>
2018-07-07 12:22:22 +0800

    commit

--- MAMP/htdocs/index.php
+++ MAMP/htdocs/index.php
@@ -3,5 +3,10 @@ echo "Hello World! \n";
 $job = 6 *5;
 echo 'test';
 echo '<br />';
-echo "good job $job";
+echo "good job $job\n";
+echo "abc" . "cdd";
+$x = "abc";
+$y = "def";
+$z = $x.$y;
+echo "$z\n";
 ?>
