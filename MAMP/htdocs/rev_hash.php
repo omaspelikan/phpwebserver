419bef22edece7a096e834175b4b446744da06c6
陳昶文 <mason@Mason-MBP.local>
2018-07-11 12:22:33 +0800

    v0.4

--- MAMP/htdocs/rev_hash.php
+++ MAMP/htdocs/rev_hash.php
@@ -4,26 +4,28 @@
     <body>
         
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
-                <input type="text" name="hashvalue">
+                <input type="text" name="md5" size=40>
                 <input type="submit" name="Submit">
             </form>
             <?php
                if ($_SERVER["REQUEST_METHOD"] == "GET") {
-                   
-                    echo "<br>"."PIN value is ".str_pad(bf_hash($_GET["hashvalue"]),4,"0",STR_PAD_LEFT);
+                    $pin = bf_hash($_GET["md5"]);
+                    if ($pin === FALSE) {
+                        echo "PIN not found.";
+                    } else {
+                        echo "PIN value is ".str_pad($pin,4,"0",STR_PAD_LEFT); 
+                    }
+                        
                } 
                function bf_hash($hashv) {
                    $found = FALSE;
                     for($i="0";$i<="9999";$i++){
                         if ($hashv == hash("md5", str_pad($i,4,"0",STR_PAD_LEFT))) {
                             $found = True;
-                            //echo "<br>"."PIN value is ". str_pad($i,4,"0",STR_PAD_LEFT);
                             return $i;
                         }
-                    }
-                    if ($found === FALSE) {
-                        echo "PIN not found.";
-                    }
+                    }                    
+                    return $found;                   
                }
             ?>     
     </body>
