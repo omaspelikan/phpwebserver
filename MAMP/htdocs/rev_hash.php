d07585a347f05ac43e36bbdf7d055cc7f8d63203
陳昶文 <mason@Mason-MBP.local>
2018-07-11 17:02:41 +0800

    add counter and time

--- MAMP/htdocs/rev_hash.php
+++ MAMP/htdocs/rev_hash.php
@@ -9,23 +9,32 @@
             </form>
             <?php
                 show_hash(15);
+                $time_start = microtime(True);
                if ($_SERVER["REQUEST_METHOD"] == "GET") {
                     $pin = bf_hash($_GET["md5"]);
                     if ($pin === FALSE) {
-                        echo "PIN not found.";
+                        echo "PIN not found."."<br>";
+                        $time_period = microtime(True) - $time_start;
+                        echo "Total time elpased: ".$time_period;
                     } else {
-                        echo "PIN value is ".str_pad($pin,4,"0",STR_PAD_LEFT); 
+                        echo "PIN value is ".str_pad($pin,4,"0",STR_PAD_LEFT)."<br>"; 
+                        $time_period = microtime(True) - $time_start;
+                        echo "Total time elpased: ".$time_period;
                     }
                         
                } 
                function bf_hash($hashv) {
                    $found = FALSE;
+                   $count = 1;
                     for($i="0";$i<="9999";$i++){
                         if ($hashv == hash("md5", str_pad($i,4,"0",STR_PAD_LEFT))) {
                             $found = True;
+                            echo "Total count ".$count."<br>";
                             return $i;
                         }
-                    }                    
+                        $count++;
+                    }
+                    echo "Total count ".$i."<br>";                    
                     return $found;                   
                }
                function show_hash($number) {
