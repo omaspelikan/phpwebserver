a65c0959568189416bd77db052b8f51b47ae0985
陳昶文 <mason@Mason-MBP.local>
2018-07-13 23:52:21 +0800

    fix login function

--- MAMP/htdocs/login.php
+++ MAMP/htdocs/login.php
@@ -1,9 +1,21 @@
 <?php
-    $oldname = isset($_POST["name"]) ? $_POST["name"] : '';
-    $oldpass = isset($_POST["pass"]) ? $_POST["pass"] : '';
-    $salt = 'XyZzy12*_';
-    $stored_hash = hash('md5', $salt.'php123');
-    $md5 = hash('md5', $_POST["pass"]);
+    //$oldname = isset($_POST["name"]) ? $_POST["name"] : '';
+    //$oldpass = isset($_POST["pass"]) ? $_POST["pass"] : '';
+    $salt = 'XyZzy12*_';                        // password = XyZzy12*_php123
+    $stored_hash = hash('md5', $salt.'php123'); // hash value of password
+    $md5 = hash('md5', $_POST["pass"]);               // hash value of user input password
+
+    function login($hash, $md5) {
+        // check login name is not empty and check password is not empty and correct.
+        // Redirect to game.php if login success.
+        if ($_POST["name"] == '' or ($_POST["pass"] == '')) {
+            echo "User name and password are required";
+        } elseif ($_POST["pass"] != '' and ($hash != $md5)) {
+            echo "Incorrect password";
+        } else {
+            echo "correct";
+        }
+    }
 ?>
 
 
@@ -19,18 +31,14 @@
 <body>
 <p class="error">
 <?php
-    if ($_POST["name"] == '' or ($_POST["pass"] == '')) {
-        echo "User name and password are required";
-    } elseif ($_POST["pass"] != '' and ($md5 != $stored_hash)) {
-        echo "Incorrect password";
-    }
+    login($stored_hash, $md5);
 ?>
 </p>
 <form method="POST">
 User Name
-<input type="text" name="name" value="<?= $oldname ?>"><br>
+<input type="text" name="name"><br>
 Password
-<input type="password" name="pass" value="<?= $oldpass ?>"><br>  
+<input type="password" name="pass"><br>  
 <input type="submit" name="Login">    
 </form>                
 </body>
