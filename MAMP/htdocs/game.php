049a55a434081b03a4676d5733dde131f58268b4
陳昶文 <mason@Mason-MBP.local>
2018-07-14 09:57:53 +0800

    some visual adjust.

--- MAMP/htdocs/game.php
+++ MAMP/htdocs/game.php
@@ -1,3 +1,11 @@
+<?php
+    if (! isset($_GET['name'])) {
+        die("Name parameter missing");
+    }
+    if (isset($_POST['exit'])) {
+        header('Location: index.php');
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,9 +15,18 @@
 <body>
 <h1>Rock Paper Scissors</h1>
 <p>
-    <?php
-        echo "Welcome ".$_GET["name"];
-    ?>
+<?php echo "Welcome ".$_GET["name"]; ?>
 </p>
+<form method="POST">
+    <select name="choice">
+        <option value="Rock">Rock</option>
+        <option value="Paper">Paper</option>
+        <option value="Scissors">Scissors</option>     
+    </select>
+    <input type="submit" value="Play" name='submit' id='buttom'>
+    <input type="submit" value="Logout" name='exit' id='buttom'>
+</form>
+<br>
+<p id='instro'>Please select a strategy and press Play.</p>
 </body>
 </html>
